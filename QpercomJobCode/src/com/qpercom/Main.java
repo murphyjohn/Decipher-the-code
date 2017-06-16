7d490f355e2639160469d55688ba0c057dc2eb3c
John Murphy <murphyjohn@users.noreply.github.com>
2017-06-16 10:00:39 +0100

    Add files via upload

---
+++ QpercomJobCode/src/com/qpercom/Main.java
@@ -0,0 +1,20 @@
+package com.qpercom;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] numArray={
+				36, 50, 97, 36, 48, 54, 36, 81, 81, 80, 111, 77, 77, 119, 53,
+        85, 109, 84, 54, 72, 75, 75, 82, 115, 122, 75, 104, 100, 117, 73,
+        57, 103, 68, 56, 75, 48, 79, 84, 70, 113, 115, 101, 48, 102, 107,
+        109, 98, 53, 111, 98, 79, 75, 47, 99, 116, 81, 50, 99, 55, 101
+        };
+		
+		for(int n:numArray){
+			char a=(char) n;
+			System.out.print(a);
+		}
+
+	}
+
+}
